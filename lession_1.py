e932626e6394d266c71f0e18f4dd21f12b40dfbe
Ayesha Shahzad <ayeshashah.ioa@gmail.com>
2023-04-24 01:22:43 +0200

    lesson 1

---
+++ lession_1.py
@@ -0,0 +1,12 @@
+ages = {"Claudia": 35, "Lisa": 52}
+print(ages)
+books = {"Stephen King": "Shinning", "Eric Ries": "The Lean Startup"}
+print(books)
+series = {"Netflix": "Dark", "Apple": "Severence", "HBO": "Game of Thrones"}
+print (series)
+dict_ages = dict({"Claudia": 35, "Lisa": 52})
+print (dict_ages)
+dict_books = dict({"Stephen King": "Shinning", "Eric Ries": "The Lean Startup"})
+print(dict_books)
+dict_series = dict({"Netflix": "Dark", "Apple": "Severence", "HBO": "Game of Thrones"})
+print (dict_series)
