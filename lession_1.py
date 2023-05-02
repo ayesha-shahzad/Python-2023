f1b995c251f814a78559b485fa7ebc1dd8b225ce
Ayesha Shahzad <ayeshashah.ioa@gmail.com>
2023-05-03 00:24:05 +0200

    Lesson 1 fix

--- lession_1.py
+++ lession_1.py
@@ -10,3 +10,14 @@ dict_books = dict({"Stephen King": "Shinning", "Eric Ries": "The Lean Startup"})
 print(dict_books)
 dict_series = dict({"Netflix": "Dark", "Apple": "Severence", "HBO": "Game of Thrones"})
 print (dict_series)
+dict_list = [ages, books, series]
+print (dict_list)
+dict_list_2 = [{"Claudia": 35, "Lisa": 52}, {"Stephen King": "Shinning", "Eric Ries": "The Lean Startup"}, {"Netflix": "Dark", "Apple": "Severence", "HBO": "Game of Thrones"}]
+print (dict_list_2)
+dict_list_3 = [dict({"Claudia": 35, "Lisa": 52}), dict({"Stephen King": "Shinning", "Eric Ries": "The Lean Startup"}), dict({"Netflix": "Dark", "Apple": "Severence", "HBO": "Game of Thrones"})]
+print (dict_list_3)
+dict_list_4 = []
+dict_list_4.append(ages)
+dict_list_4.append(books)
+dict_list_4.append(series)
+print (dict_list_4)
