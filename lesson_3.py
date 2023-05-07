8a734a151e92b1f34cfccc0fc349cf2180967a30
Ayesha Shahzad <ayeshashah.ioa@gmail.com>
2023-05-08 00:10:25 +0200

    lesson 3 assignment 1

---
+++ lesson_3.py
@@ -0,0 +1,15 @@
+temp = int(input('Please provide temperature today: '))
+
+if temp < 12 : print('wear a jacket')
+
+if temp < 6 : print('wear a pair of gloves')
+
+if temp < 10 : print('wear a scarf')
+
+if temp < 8 : print('wear a hat')
+
+if temp > 25 : print('wear a sunglasses')
+
+rain = int(input('Please provide chance of rain today: '))
+
+if rain >= 40 : print('use an umbrella')
